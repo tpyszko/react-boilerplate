115654daec3b0290ed9199b6eb0834145e96b385
Tomek <tomasz.pyszko@gmail.com>
2019-02-05 13:30:44 +0100

    Add webpack dev server and html plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("index.js")
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./container/App"
+
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
